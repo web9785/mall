57e3ec64c26c04c852c15238c06cfdbb141d7b57
hdy <hdy@163.com>
2021-05-21 17:50:16 +0800

    navbar

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="nav-bar">
+    <div class="nav-left">
+        <slot name="nav-left"></slot>
+    </div>
+    <div class="nav-center">
+        <slot name="nav-center"></slot>
+    </div>
+    <div class="nav-right">
+        <slot name="nav-right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style scoped>
+    .nav-bar{
+        display: flex;
+        /* 导航栏一般高度为44px */
+        height: 44px;
+        line-height: 44px;
+        text-align: center;
+    }
+    .nav-left, .nav-right{
+        width:60px;
+    }
+    .nav-center{
+        flex:1;
+    }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,24 +1,30 @@
 <template>
-  <div>
-    <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="nav-center">
+        购物街
+      </div>
+    </nav-bar>
   </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar"
 export default {
   name: 'Home',
   data () {
     return {
     }
   },
-
-  methods: {},
-
-  created () {
+  components:{
+    NavBar
   }
 }
 </script>
 
 <style scoped>
-
+  .home-nav{
+    background: var(--color-tint);
+    color:#fff;
+  }
 </style>
